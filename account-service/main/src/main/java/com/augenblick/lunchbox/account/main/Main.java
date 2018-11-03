9c710b162f1c065afb9a043e39120065f1e2f479
Mohamed Khorshid <mohamedkhorshid@Mohameds-MacBook-Pro-2.local>
2018-11-03 12:51:23 +0400

    support docker

--- account-service/main/src/main/java/com/augenblick/lunchbox/account/main/Main.java
+++ account-service/main/src/main/java/com/augenblick/lunchbox/account/main/Main.java
@@ -1,21 +1,13 @@
 package com.augenblick.lunchbox.account.main;
 
-import java.util.Arrays;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import com.augenblick.lunchbox.account.rest.aspects.RestAspect;
 
 @ComponentScan(basePackages = {
 		"com.augenblick.lunchbox.account.rest",
@@ -30,7 +22,7 @@ import com.augenblick.lunchbox.account.rest.aspects.RestAspect;
 public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Main.class, args);
+		 SpringApplication.run(Main.class, args);
 	}
 
 	@Bean
